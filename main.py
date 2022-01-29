b753772fe7c2bab95b066b011272b99e94d89645
James Lo <llotszfor12346@gmail.com>
2022-01-29 17:37:24 +0800

    with redis

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import time
 from flask import Flask
+
+import redis
+
+r = redis.Redis(
+    host='10.182.0.2',
+    port=6379)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p> This is a small testing of using Redis</p>"
+
+@app.route("/<x>")
+def hello_world(x):
+
+    if not r.exist(x):
+
+        result = x + 1
+        time.sleep(5)
+        return result
+        r.set(result)
+
+    else:
+        r.get(x)
